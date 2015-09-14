3d5423d7d277504dd14331c716be39ca28ff69fa
Andrew Browne <brownie@brownie.com.au>
2015-09-14 12:54:21 +0000

    Perf tweaks

--- s32dynamo/project.clj
+++ s32dynamo/project.clj
@@ -7,7 +7,6 @@
                  [amazonica "0.3.33"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [metrics-clojure "2.5.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.1"]
                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot s32dynamo.core
   :target-path "target/%s"
